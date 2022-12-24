91e2f36137c54de79af0adcf6392f8ba117917a8
eowiz <eowiz@proton.me>
2022-12-24 19:10:20 +0900

    Modify Lazy

--- src/main/java/pebble/data/Lazy.java
+++ src/main/java/pebble/data/Lazy.java
@@ -1,5 +1,6 @@
 package pebble.data;
 
+import java.util.Objects;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import org.jetbrains.annotations.NotNull;
@@ -8,14 +9,14 @@ import org.jetbrains.annotations.Nullable;
 public final class Lazy<T> implements Supplier<T> {
 
   @NotNull
-  private final Supplier<@Nullable T> supplier;
+  private final Supplier<? extends @Nullable T> supplier;
 
   @Nullable
   private T value;
 
   private boolean getCalled;
 
-  private Lazy(@NotNull final Supplier<@Nullable T> supplier) {
+  private Lazy(@NotNull final Supplier<? extends @Nullable T> supplier) {
     this.supplier = supplier;
   }
 
@@ -45,5 +46,33 @@ public final class Lazy<T> implements Supplier<T> {
   public <U> Lazy<U> flatMap(@NotNull
       final Function<? super @Nullable T, @NotNull Lazy<? extends @Nullable U>> mapping) {
     return (Lazy<U>) mapping.apply(this.value);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+
+    if (!(obj instanceof Lazy<?>)) {
+      return false;
+    }
+
+    final var lazy = (Lazy<?>) obj;
+
+    if (this.getCalled != lazy.getCalled) {
+      return false;
+    }
+
+    if (!Objects.equals(this.supplier, lazy.supplier)) {
+      return false;
+    }
+
+    return Objects.equals(value, lazy.value);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(this.supplier, this.getCalled, this.value);
   }
 }
