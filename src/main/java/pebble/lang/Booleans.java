644f771c64d58f24b2281c97a2c52cbfe5d7e126
eowiz <eowiz@proton.me>
2022-12-25 16:17:50 +0900

    Add methods to Booleans

--- src/main/java/pebble/lang/Booleans.java
+++ src/main/java/pebble/lang/Booleans.java
@@ -3,4 +3,20 @@ package pebble.lang;
 public final class Booleans {
 
   private Booleans() {}
+
+  public static boolean isTrue(Boolean bool) {
+    return Boolean.TRUE.equals(bool);
+  }
+
+  public static boolean isTrue(boolean bool) {
+    return bool;
+  }
+
+  public static boolean isFalse(Boolean bool) {
+    return Boolean.FALSE.equals(bool);
+  }
+
+  public static boolean isFalse(boolean bool) {
+    return !bool;
+  }
 }
