c69550032ee7a033b302a87bda347c4c8e22336c
eowiz <eowiz@proton.me>
2022-12-29 19:28:26 +0900

    Add test for Functions

--- src/main/java/pebble/lang/Functions.java
+++ src/main/java/pebble/lang/Functions.java
@@ -9,6 +9,7 @@ public final class Functions {
 
   private Functions() {}
 
+  @NotNull
   public static <T, U> Function<@Nullable T, @Nullable U> safety(
       @NotNull Function<@NotNull T, @Nullable U> f) {
     Objects.requireNonNull(f);
---
+++ src/test/java/pebble/lang/FunctionsTest.java
@@ -0,0 +1,33 @@
+package pebble.lang;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.ThrowableAssert.catchThrowable;
+
+import java.util.function.Function;
+import org.junit.jupiter.api.Test;
+import pebble.data.Option;
+
+public class FunctionsTest {
+
+  @Test
+  public void safety_null_throws_exception() {
+    // act:
+    final var actual = catchThrowable(() -> Functions.safety(null));
+
+    // assert:
+    assertThat(actual).isInstanceOf(NullPointerException.class);
+  }
+
+  @Test
+  public void safety_apply_to_full() {
+    // arrange:
+    Function<Integer, String> f = Object::toString;
+
+    // act:
+    final var actualSafety = Functions.safety(f);
+    final var actual = actualSafety.apply(null);
+
+    // assert:
+    assertThat(actual).isNull();
+  }
+}
