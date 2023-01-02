5aac819713e9d036f9c27e5aabd9f4539ff72c25
eowiz <eowiz@proton.me>
2023-01-03 02:10:57 +0900

    Add a function to Filez

--- src/main/java/pebble/io/Filez.java
+++ src/main/java/pebble/io/Filez.java
@@ -1,5 +1,23 @@
 package pebble.io;
 
+import java.io.File;
+import java.nio.file.NotDirectoryException;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class Filez {
 
+  private Filez() {}
+
+  public static Stream<Path> listPaths(String first, String... more) throws NotDirectoryException {
+    final var file = Path.of(first, more).toFile();
+    final var files = file.listFiles();
+
+    if (files == null) {
+      throw new NotDirectoryException(file.toString());
+    }
+
+    return Arrays.stream(files).map(File::toPath);
+  }
 }
